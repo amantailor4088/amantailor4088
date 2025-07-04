655f6025ca90431d2d34daf97bb46622b73a0456
Devansh <devanshjindal31@gmail.com>
2025-07-04 10:13:27 +0530

    hosting changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   api: {
     bodyParser: {
-      sizeLimit: "1mb", // increase as needed
+      sizeLimit: "1mb",
     },
   },
 };
