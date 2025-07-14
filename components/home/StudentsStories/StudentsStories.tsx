c86de4992da9b03d74f6df53b12b7aa9ad410fa7
Devansh <devanshjindal31@gmail.com>
2025-07-14 16:07:29 +0530

    updated reviews image

--- components/home/StudentsStories/StudentsStories.tsx
+++ components/home/StudentsStories/StudentsStories.tsx
@@ -8,22 +8,36 @@ import "swiper/css/pagination";
 
 const students = [
     {
-        name: "Simran Kaur",
-        image: "/sample.png",
-        quote:
-            "The online course gave me confidence to start my own boutique. I now earn from what I love!",
+        image: "/reviews/1.jpg",
+        quote: "The online course gave me confidence to start my own boutique. I now earn from what I love!"
     },
     {
-        name: "Ravi Kumar",
-        image: "/sample.png",
-        quote:
-            "In just 3 months, I went from zero to tailoring professionally. And all in Punjabi!",
+        image: "/reviews/2.jpg",
+        quote: "In just 3 months, I went from zero to tailoring professionally. And all in Punjabi!"
     },
     {
-        name: "Ayesha Sheikh",
-        image: "/sample.png",
-        quote:
-            "Stitching is now my side hustle, all thanks to the easy video lessons and flexible timing.",
+        image: "/reviews/3.jpg",
+        quote: "Stitching is now my side hustle, all thanks to the easy video lessons and flexible timing."
+    },
+    {
+        image: "/reviews/4.jpg",
+        quote: "I never thought I could stitch, but this course made it so simple and enjoyable!"
+    },
+    {
+        image: "/reviews/6.jpg",
+        quote: "The instructors are so patient and the content is incredibly clear. My skills have truly transformed."
+    },
+    {
+        image: "/reviews/7.jpg",
+        quote: "This online course fit perfectly into my busy schedule, allowing me to pursue my passion for fashion."
+    },
+    {
+        image: "/reviews/8.jpg",
+        quote: "From basic stitches to complex designs, every lesson was a step towards becoming a confident tailor."
+    },
+    {
+        image: "/reviews/5.jpg",
+        quote: "United by our success, we're proud to be part of this incredible journey, showing that anyone can achieve their dreams here!"
     },
 ];
 
@@ -37,7 +51,7 @@ const SuccessStoriesSlider = () => {
 
                 <Swiper
                     modules={[Pagination, Autoplay]}
-                    autoplay={{ delay: 6000 }}
+                    autoplay={{ delay: 9000 }}
                     loop
                     pagination={{
                         clickable: true,
@@ -49,16 +63,14 @@ const SuccessStoriesSlider = () => {
                     {students.map((student, index) => (
                         <SwiperSlide key={index}>
                             <div className="relative flex flex-col lg:flex-row items-center gap-10 bg-white/90 dark:bg-neutral-800/90 p-8 sm:p-10 rounded-2xl shadow-xl">
-                                <div className="w-full lg:w-1/2 relative h-64 sm:h-92 overflow-hidden">
+                                <div className="w-full lg:w-1/2 relative h-64 sm:h-92 overflow-hidden transition-transform duration-300 ease-in-out hover:scale-110 rounded-md">
                                     <Image
                                         src={student.image}
-                                        alt={student.name}
+                                        alt={"image not found"}
                                         fill
                                         className="object-contain object-top"
                                     />
                                 </div>
-
-                                {/* Text with background quote */}
                                 <div className="w-full lg:w-1/2 relative z-10 text-center lg:text-left min-h-[16rem] sm:min-h-[20rem] flex items-center">
                                     <div className="absolute -top-0 lg:top-5 -left-5 lg:-left-18 text-[7rem] sm:text-[9rem] leading-none font-serif text-purple-500 dark:text-purple-300 opacity-20 select-none z-0 pointer-events-none">
                                         “
@@ -67,11 +79,8 @@ const SuccessStoriesSlider = () => {
                                         <p className="text-lg sm:text-xl font-medium text-gray-700 dark:text-gray-100 mb-4 leading-relaxed">
                                             {student.quote}
                                         </p>
-                                        <h4 className="text-base font-semibold text-purple-700 dark:text-purple-300">
-                                            — {student.name}
-                                        </h4>
                                     </div>
-                                     <div className="absolute bottom-0 right-0 md:bottom-5 md:right-10 text-[7rem] sm:text-[9rem] leading-none font-serif text-purple-500 dark:text-purple-300 opacity-20 select-none z-0 pointer-events-none rotate-180">
+                                    <div className="absolute bottom-0 right-0 md:bottom-5 md:right-10 text-[7rem] sm:text-[9rem] leading-none font-serif text-purple-500 dark:text-purple-300 opacity-20 select-none z-0 pointer-events-none rotate-180">
                                         “
                                     </div>
                                 </div>
